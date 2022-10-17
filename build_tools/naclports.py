9109e54eb734e928c2d2800648dda995a7b324d0
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-17 09:11:59 +0000

    Adding tarfile member sanitization to extractall()

--- build_tools/naclports.py
+++ build_tools/naclports.py
@@ -287,7 +287,26 @@ class BinaryPackage(object):
           if name not in ('.', 'pkg_info', 'payload'):
             if not name.startswith(PAYLOAD_DIR):
               raise Error('invalid file in package: %s' % name)
-        t.extractall(dest_tmp)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(t, dest_tmp)
 
       for name in names:
         self.InstallFile(name, dest_tmp, dest)
